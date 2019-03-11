481c328af0c911093aa2509f0c56377cc9901d3a
livirhind <livi.rhind@gmail.com>
2019-03-11 11:09:42 -0400

    adding initialise and mapfunctions

---
+++ initialise.js
@@ -0,0 +1,21 @@
+function loadW3HTML() {
+	w3.includeHTML();
+}
+
+
+function startup() {
+	document.addEventListener('DOMContentLoaded', function(){
+		getPort();
+	},false);
+	document.addEventListener('DOMContentLoaded', function(){
+		zoomOnMap();
+	},false)
+	document.addEventListener('DOMContentLoaded', function(){
+		loadW3HTML();
+	},false);
+	document.addEventListener('DOMContentLoaded', function(){
+		startFormDataLoad();
+	},false);
+}
+
+
