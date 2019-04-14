fb796cceecbff7d55d5cb6910f494eb3cf2ef8cc
livirhind <livi.rhind@gmail.com>
2019-04-14 05:04:39 -0400

    Editing startup so the map zooms in on user's location

--- startup.js
+++ startup.js
@@ -1,9 +1,11 @@
 
-
+//Function startup designed to automatically zoom in on the user's location when the quiz app is launched and load the quizpoints 
+//Reference: zoomOnMap taken from Practical 2 (Location based services)
+//getQuizPoints taken from Practical 6 and 7 
 function startup() {
-	//document.addEventListener('DOMContentLoaded', function(){
-		//zoomOnMap();
-	//},false)
+	document.addEventListener('DOMContentLoaded', function(){
+		zoomOnMap();
+	},false)
 	
 	document.addEventListener('DOMContentLoaded', function(){
 		getQuizPoints();
